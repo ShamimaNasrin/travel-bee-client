ae84449fa99333f698163f95aa6dd850dc82b525
ShamimaNasrin <shamima.nasrin007@gmail.com>
2022-12-26 17:33:30 +0600

    initial installations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Travel Bee</h1>
+      <button className="btn btn-active btn-secondary">Button</button>
+      <button className="btn btn-active btn-accent">Button</button>
+      <button className="btn btn-active btn-ghost">Button</button>
     </div>
   );
 }
